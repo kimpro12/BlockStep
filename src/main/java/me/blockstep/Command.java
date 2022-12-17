a485f5b2c3d8fa766796e09da2ccd6db2e347cf5
kimpro12 <95573884+kimpro12@users.noreply.github.com>
2022-12-17 00:01:58 -0800

    Add files via upload

--- src/main/java/me/blockstep/Command.java
+++ src/main/java/me/blockstep/Command.java
@@ -1,17 +1,13 @@
 package me.blockstep;
 
 import org.bukkit.Bukkit;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -37,45 +33,40 @@ public class Command implements CommandExecutor, Listener {
                 }
                 if (args.length == 1) {
                     if (args[0].equalsIgnoreCase("help")) {
-                        Sendmessage();
+                        Sendmessage(sender);
                     }
                     Player target = Bukkit.getPlayer(args[0]);
                     if (args[0].equalsIgnoreCase("all")) {
                         for (Player p : Bukkit.getOnlinePlayers()) {
-                            if (map.containsKey(p.getUniqueId())) {
-                                Bukkit.getScheduler().cancelTask(map.get(p.getUniqueId()));
-                                map.remove(p.getUniqueId());
-                            }
-                            p.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Can-BlockStep")));
-                            Dotask(p);
+                            return CheckMap(p);
                         }
                     } else if (target != null) {
-                        if (map.containsKey(target.getUniqueId())) {
-                            Bukkit.getScheduler().cancelTask(map.get(target.getUniqueId()));
-                            map.remove(target.getUniqueId());
-                        }
-                        target.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Can-BlockStep")));
-                        Dotask(target);
+                        return CheckMap(target);
                     } else if (!args[0].equalsIgnoreCase("help")){
                         sender.sendMessage(color.transalate(t + " " + plugin.getConfig().getString("Message-When-Player-Don't-Exist")));
+                        return true;
                     }
                 }
                 if (args.length == 2) {
                     if (args[0].equalsIgnoreCase("stop")) {
-                        Player target2 = Bukkit.getPlayer(args[1]);
                         for (Player p : Bukkit.getOnlinePlayers()) {
-                            if (args[1].equalsIgnoreCase("all") && map.containsKey(p.getUniqueId())) {
-                                Bukkit.getScheduler().cancelTask(map.get(p.getUniqueId()));
-                                map.remove(p.getUniqueId());
-                                p.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Cannot-BlockStep")));
-                            } else if (target2 != null && map.containsKey(target2.getUniqueId())) {
-                                Bukkit.getScheduler().cancelTask(map.get(target2.getUniqueId()));
-                                map.remove(target2.getUniqueId());
-                                target2.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Cannot-BlockStep")));
-                            } else {
-                                sender.sendMessage(color.transalate(t + " " + plugin.getConfig().getString("Message-When-Player-Don't-Exist")));
-                            }
-                        }
+                             if (args[1].equalsIgnoreCase("all") && map.containsKey(p.getUniqueId())) {
+                                 Bukkit.getScheduler().cancelTask(map.get(p.getUniqueId()));
+                                 map.remove(p.getUniqueId());
+                                 p.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Cannot-BlockStep")));
+                                 return true;
+                             }
+                             Player target2 = Bukkit.getPlayer(args[1]);
+                             if (target2 != null && map.containsKey(target2.getUniqueId()) && !(args[1].equalsIgnoreCase("all"))) {
+                                 Bukkit.getScheduler().cancelTask(map.get(target2.getUniqueId()));
+                                 map.remove(target2.getUniqueId());
+                                 target2.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Cannot-BlockStep")));
+                                 return true;
+                             } else if (target2 == null && !(args[1].equalsIgnoreCase("all"))) {
+                                 sender.sendMessage(color.transalate(t + " " + plugin.getConfig().getString("Message-When-Player-Don't-Exist")));
+                                 return true;
+                             }
+                         }
                     }
                 }
             }
@@ -85,23 +76,26 @@ public class Command implements CommandExecutor, Listener {
         return true;
     }
 
-    private void Dotask(Player p) {
-        map.put(p.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new BukkitRunnable() {
-            @Override
-            public void run() {
-                Block block = p.getLocation().getBlock().getRelative(BlockFace.DOWN);
-                ItemStack item = new ItemStack(block.getType());
-                p.getInventory().addItem(item);
-            }
-        }, 0, plugin.getConfig().getInt("Second") * 20L));
+    private boolean CheckMap(Player p) {
+        if (map.containsKey(p.getUniqueId())) {
+            Bukkit.getScheduler().cancelTask(map.get(p.getUniqueId()));
+            map.remove(p.getUniqueId());
+        }
+        p.sendMessage(color.transalate(plugin.getConfig().getString("When-SomeOne-Can-BlockStep")));
+        Dotask(p);
+        return true;
     }
 
-    private void Sendmessage() {
-        Bukkit.getConsoleSender().sendMessage("---------------------BlockStep--------------------");
-        Bukkit.getConsoleSender().sendMessage("/blockstep all to make all player can blockstep");
-        Bukkit.getConsoleSender().sendMessage("/blockstep <player> to make <player> can blockstep");
-        Bukkit.getConsoleSender().sendMessage("/blockstep stop all player can blockstep");
-        Bukkit.getConsoleSender().sendMessage("/blockstep stop <player> to stop <player> can blockstep");
+    public void Dotask(Player p) {
+        map.put(p.getUniqueId(), Bukkit.getScheduler().runTaskTimer(plugin, new Task(plugin, p), 0, plugin.getConfig().getInt("Second") * 20L).getTaskId());
+    }
+
+    private void Sendmessage(CommandSender sender) {
+        sender.sendMessage("---------------------BlockStep--------------------");
+        sender.sendMessage("/blockstep all to make all player can blockstep");
+        sender.sendMessage("/blockstep <player> to make <player> can blockstep");
+        sender.sendMessage("/blockstep stop all player can blockstep");
+        sender.sendMessage("/blockstep stop <player> to stop <player> can blockstep");
     }
 
     @EventHandler
